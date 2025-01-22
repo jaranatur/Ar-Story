52f9f0e0e158945a442d86fe9fc60d3a868e91b0
jaranatur <64973941+jaranatur@users.noreply.github.com>
2025-01-22 11:08:09 +0100

    new selectOption()

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// Funktion zur Verarbeitung der Auswahl
 function selectOption(option, co2Value) {
-    // Feedback aktualisieren
-    const feedback = document.getElementById('feedback');
-    feedback.setAttribute(
-      'text',
-      `value: Du hast ${option} gewählt. Dein CO₂-Ausstoß beträgt ${co2Value} kg.`
-    );
-  }
-  
+  const feedback = document.querySelector("#feedback");
+  feedback.setAttribute(
+    "text",
+    `value: Du hast ${option} gewählt. CO₂-Ausstoß: ${co2Value} kg.; color: black; align: center;`
+  );
+}
