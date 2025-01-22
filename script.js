f3ebb7cbe7bbae1e069bbab84a8906a6293379f8
jaranatur <64973941+jaranatur@users.noreply.github.com>
2025-01-22 11:52:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function selectOption(option, co2Value) {
+  
+ console.log(`Option ${option} wurde ausgewählt mit CO₂-Wert: ${co2Value}`);
+  
   const feedback = document.querySelector("#feedback");
   feedback.setAttribute(
     "text",
