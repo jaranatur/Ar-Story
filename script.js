0f4baf2558a9344cd94aa4b1978eb74edc9b251e
jaranatur11 <jara.natur@t-online.de>
2025-01-23 23:46:34 +0100

    back to normal

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 // Sicherstellen, dass die Szene vollständig geladen ist, bevor Event-Listener hinzugefügt werden
 document.addEventListener("DOMContentLoaded", () => {
-  // Begrüßungstext und Frage-Element auswählen
+  // Begrüßungstext und Erklärungstext
   const startScreen = document.querySelector("#start-screen");
   const clickableText = document.querySelector("#clickable-text");
   const question = document.querySelector("#question");
 
   // Event-Listener für "Klicke HIER"
   clickableText.addEventListener("click", () => {
-    console.log("Text wurde angeklickt!");
     // Begrüßungstext ausblenden
     startScreen.setAttribute("visible", "false");
 
     // Erste Frage anzeigen
     question.setAttribute("visible", "true");
+  });
+
+  // Event-Listener für Antwortoptionen
+  document.querySelector("#bike-option").addEventListener("click", () => {
+    alert("Du hast Fahrrad gewählt!");
+  });
+
+  document.querySelector("#bus-option").addEventListener("click", () => {
+    alert("Du hast Bus gewählt!");
+  });
+
+  document.querySelector("#car-option").addEventListener("click", () => {
+    alert("Du hast Auto gewählt!");
   });
 });
