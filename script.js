348f571b410bd4d7dde9b0312b7724b31ca914a8
jaranatur11 <jara.natur@t-online.de>
2025-01-23 20:39:35 +0100

    ?????

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function startQuiz() {
   const startScreen = document.querySelector("#start-screen");
   if (startScreen) startScreen.setAttribute("visible", "false");
 
-  // Zeige die Frage
+  // Zeigt die Frage
   const question = document.querySelector("#question");
   question.setAttribute("visible", "true");
 
-  // Zeige die Transportmittel
+  // Zeigt die Transportmittel
   document.querySelector("#fahrrad").setAttribute("visible", "true");
   document.querySelector("#oepnv").setAttribute("visible", "true");
   document.querySelector("#auto").setAttribute("visible", "true");
@@ -27,19 +27,4 @@ function selectOption(option, co2Value) {
     `value: Du hast ${option} gewählt. CO₂-Ausstoß: ${co2Value} kg.; color: black; align: center;`
   );
   feedback.setAttribute("visible", "true");
-
-  // Zeigt ein Diagramm oder Balken mit dem CO₂-Wert
-  showDiagram(option, co2Value);
-}
-
-// Funktion zum Anzeigen eines Diagramms oder einer Visualisierung
-function showDiagram(option, co2Value) {
-  const diagram = document.querySelector("#diagram");
-  const diagramText = `CO₂-Ausstoß: ${co2Value} kg \n(${option})`;
-
-  diagram.setAttribute(
-    "text",
-    `value: ${diagramText}; color: black; align: center; wrapCount: 20; font: mozillavr`
-  );
-  diagram.setAttribute("visible", "true");
 }
